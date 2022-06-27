7d8bcf6386c5fa2c5da57e183d39894fffd5a2a1
godwin opara <godwinopara62@yahoo.com>
2022-06-27 21:37:52 +0100

    create a sentence with string slicing

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+obj = str.split()
+print(f"{obj[5]} {obj[6]} {obj[12]} {obj[0]}")
