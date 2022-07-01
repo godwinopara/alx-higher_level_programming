4191d5a565adcda6a4ac87d7244f8b61186dd31d
godwin opara <godwinopara62@yahoo.com>
2022-07-01 14:33:10 +0100

    import a function that adds two number

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from add_0 import add
-a = 1
-b = 2
-c = add(a, b)
-print(f"{a:d} + {b:d} = {c}")
+
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
